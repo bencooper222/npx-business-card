a21592e5566754f8bfdc5ad344f3c1bffae29fc4
Benjamin Cooper <bencooper222@gmail.com>
2018-12-22 14:53:18 -0600

    Cleaned up generic card code

--- bin/card.js
+++ bin/card.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
-
-'use strict'
-
-const fs = require('fs')
-const path = require('path')
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
+'use strict';
+console.log(
+  require('fs').readFileSync(require('path').join(__dirname, 'output'), 'utf8'),
+);
